9a5eac1a4c07820f66b32f455d9d303ba478c49b
Omar Shaker <omarshaker082@gmail.com>
2026-03-18 14:10:22 +0200

    Finished exception file classes

--- src/game/engine/exceptions/GameActionException.java
+++ src/game/engine/exceptions/GameActionException.java
@@ -1,14 +1,13 @@
 package game.engine.exceptions;
 
 public abstract class GameActionException extends Exception{
-		/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 		public GameActionException()
 		{
 			super();
 		}
+
 		public GameActionException(String message)
 		{
 			super(message);
